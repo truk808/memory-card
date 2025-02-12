3e1872d45611565a4cc20f69e0622596c67a2ac8
truk808 <ilay.shulga.ru@gmail.com>
2025-02-12 12:36:04 +0300

    Создание проекта и подключение к бд

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const start = async () => {
+    try {
+        await sequelize.authenticate() // устанавливается подключение к бд
+        await  sequelize.sync() // сверяет состояние бд со схемой данных
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        })
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+
+start()
