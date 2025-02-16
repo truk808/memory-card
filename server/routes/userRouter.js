24420e4561b97bfcab132726c59d484b40daca3a
truk808 <ilay.shulga.ru@gmail.com>
2025-02-16 17:22:38 +0300

    Создание авторизации

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const {Router} = require("express");
 const router = new Router()
 const UserController = require("../controllers/UserController");
+const authMiddleware = require("../middleware/authmiddleware");
 
 router.post('/registration', UserController.registration)
 router.post('/login', UserController.login)
-router.get('/auth', UserController.check)
+router.get('/auth', authMiddleware, UserController.check)
 
 module.exports  = router
