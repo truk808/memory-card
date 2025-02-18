6302496364cd7699ce94954e51228433a0218cd7
truk808 <ilay.shulga.ru@gmail.com>
2025-02-18 19:22:45 +0300

    Создание headera

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import React from "react";
+import './style/app.css'
 
 function App() {
   return (
--- client/src/page/About.jsx
+++ client/src/page/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from '../components/header/Header';
+import Footer from "../components/footer/Footer";
 
 const About = () => {
     return (
         <div>
-            About
+            <Header />
+            <main className="main">
+                <div className="">
+                    <h1></h1>
+                </div>
+            </main>
+            {/*<Footer />*/}
         </div>
     );
 };
